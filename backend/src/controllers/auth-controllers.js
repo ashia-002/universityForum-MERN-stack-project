c98e19e4d6245759d9516f5a9bdea046920300de
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2025-10-08 20:45:18 +0600

    Added auth Controllers

---
+++ backend/src/controllers/auth-controllers.js
@@ -0,0 +1,117 @@
+const User = require('../models/user.js')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const user = require('../models/user.js')
+const Department = require('../models/department.js')
+require('dotenv').config()
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password, role, department_id } = req.body;
+
+        if(!email.endsWith('@rpsu.edu.bd')){
+            return res.status(400).json({
+                message: 'Only university emails (rpsu.edu.bd) are alowed'
+            })
+        }
+
+        const exists = await user.findOne({email});
+        if (exists) return res.status(400).json({
+            message: 'User already exists'
+        });
+
+        const department = await Department.findById(department_id);
+        if (!department) {
+        return res.status(400).json({ message: 'Invalid department ID' });
+        }
+
+        const newUser = new User({
+            user_id: `U-${Date.now()}`,
+            name,
+            email,
+            password,
+            role,
+            department_id
+        });
+
+        await newUser.save();
+
+        await Department.findByIdAndUpdate(
+      department_id,
+      { $addToSet: { users: newUser._id } }, // prevents duplicates
+      { new: true }
+    );
+
+        res.status(201).json({
+            message: 'user registered successfully'
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error Registering User',
+            error: error.message
+        });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        
+        const user = await User.findOne({email});
+        if(!user) return res.status(404).json({
+            message: 'User not registered'
+        });
+
+        const validPassward = await bcrypt.compare(password, user.password);
+        if(!validPassward) return res.status(400).json({
+            message: 'Invalid Password'
+        });
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            JWT_SECRET,
+            { expiresIn: '7d' }
+        );
+
+            // Set the token in a cookie with dynamic `secure` based on the environment
+            res.cookie("token", token, {
+                httpOnly: false,  // Ensures the cookie is not accessible via JavaScript
+                secure: process.env.NODE_ENV === "production",  // Use `secure: true` in production (HTTPS)
+                sameSite: "Strict",  // Ensures the cookie is only sent in requests from the same origin
+                maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            });
+
+        res.json({
+            message: 'Login Successful',
+            token,
+            user: {
+                name: user.name,
+                role: user.role,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error logging in',
+            error: error.message,
+        })
+    }
+}
+
+exports.logout = (req, res) => {
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV == 'production',
+        sameSite: 'strict',
+    });
+    res.status(200).json({
+        message: 'Logged out successfully'
+    });
+}
