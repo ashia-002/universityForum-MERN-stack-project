3f68575f40f0742eb4b0c6a280ffca646d384d3c
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2025-10-22 19:48:54 +0600

    Made changes in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,6 +7,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // ✅ Automatically attach JWT token (if available) to all requests
