dedd54b4863a05da3a531fff47b6ba10a31e34a3
farhanaz-08 <firstuser347@gmail.com>
2025-10-22 17:37:47 +0600

    i cant believe casing could cause this much touble

--- frontend/src/components/layout/Dashboard.jsx
+++ frontend/src/components/layout/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import CreatePost from './CreatePost';
-import PostCard from './Postcard'
+import Postcard from './Postcard'
 
 const Dashboard = () => {
   const [showCreatePost, setShowCreatePost] = useState(false);
