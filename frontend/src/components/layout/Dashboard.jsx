c021ece9f37792e5563fdcfa2e8f4ed8e625d2b6
farhanaz-08 <firstuser347@gmail.com>
2025-10-22 17:19:29 +0600

    post card path fixing

--- frontend/src/components/layout/Dashboard.jsx
+++ frontend/src/components/layout/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import CreatePost from './CreatePost';
-import PostCard from '../layout/Postcard';
+import PostCard from './Postcard';
 
 const Dashboard = () => {
   const [showCreatePost, setShowCreatePost] = useState(false);
