5a84f64937f61fd8e4e40fab96fd61ae7913caf3
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2025-11-02 21:24:08 +0600

    Changed server.js.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const authRoutes = require('../backend/src/routes/auth-routes.js');
 const deptRoutes = require('../backend/src/routes/dept-routes.js');
 const postRoutes = require('../backend/src/routes/post-routes.js');
 const eventRoutes = require('../backend/src/routes/event-routes.js');
-//const announcementRoutes = require('../backend/src/routes/announcement-routes.js')
+const announcementRoutes = require('../backend/src/routes/announcement-routes.js');
+const communityRoutes = require('../backend/src/routes/community-routes.js');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -57,7 +58,8 @@ app.use('/auth', authRoutes);
 app.use('/get/department', deptRoutes);
 app.use('/post', postRoutes);
 app.use('/event', eventRoutes);
-//app.use('/announcement', announcementRoutes)
+app.use('/announcement', announcementRoutes);
+app.use('/community', communityRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
