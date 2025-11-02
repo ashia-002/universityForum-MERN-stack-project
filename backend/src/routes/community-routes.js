e8654c5addbfc26792461e1417b1c090b8b8f6f0
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2025-11-02 21:23:43 +0600

    Added community routes.

---
+++ backend/src/routes/community-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { authentication } = require('../middlewares/authentication');
+const communityController = require('../controllers/community-controllers');
+
+
+router.post('/create', authentication, communityController.createCommunity);
+router.put('/edit/:id', authentication, communityController.editCommunity);
+router.delete('/delete/:id', authentication, communityController.deleteCommunity);
+
+// View & Fetch
+router.get('/view/:id', communityController.getCommunityById); 
+router.get('/:id/posts', communityController.getCommunityPosts);
+router.get('/:id/events', communityController.getCommunityEvents);
+router.get('/:id/announcements', communityController.getCommunityAnnouncements);
+
+module.exports = router;
